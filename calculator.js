6a835d9ce73fcea31db53708b644682d536e04fa
dprentice <deberohp@gmail.com>
2023-07-29 17:41:29 -0400

    working on logic to evaluate addition

--- calculator.js
+++ calculator.js
@@ -1,9 +1,33 @@
 const number = document.querySelectorAll(".num")
-const numbers = document.querySelectorAll(".numbers")
+// const displayNumbers = document.querySelectorAll(".display-numbers")
 const display = document.querySelector(".display")
+const symbol = document.querySelectorAll(".symbol")
+const equal = document.querySelector(".equal")
+let result
 
 number.forEach(num => {
-    num.addEventListener("click", e => {
-        display.innerText += num.innerText
+    
+    num.addEventListener("click", () => {
+        display.innerText = result
+    })
+    
+})
+
+
+
+symbol.forEach(sym => {
+    sym.addEventListener("click", () => {
+       
+        display.innerText += sym.innerText
     })
 })
+
+
+
+function add(...args) {
+    return args.reduce(function(acc, curr) {
+        return acc + curr
+    })
+}
+
+
