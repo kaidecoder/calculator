1ae20790afd2c80170e9f82c437837ab180b65c1
dprentice <deberohp@gmail.com>
2023-07-29 18:33:37 -0400

    the add function works.  need logic to call the add or multiply function based on the symbol

--- calculator.js
+++ calculator.js
@@ -3,30 +3,49 @@ const number = document.querySelectorAll(".num")
 const display = document.querySelector(".display")
 const symbol = document.querySelectorAll(".symbol")
 const equal = document.querySelector(".equal")
-let result
+let addends = []
 
 number.forEach(num => {
-    
     num.addEventListener("click", () => {
-        display.innerText = result
+        addends.push(Number(num.innerText))
+        display.innerText += num.innerText
+        
     })
-    
+
 })
-
-
 
 symbol.forEach(sym => {
     sym.addEventListener("click", () => {
-       
         display.innerText += sym.innerText
     })
+    equal.addEventListener("click", () => {
+        if (symbol.innerText == '&plus;') {
+            let sum = add(...addends)
+            display.innerText = sum
+        }else if ( symbol === "*"){
+            let product = multiply(...addends)
+            display.innerText = product
+        }
+        
+    })
+
 })
+
+
+
+
+
 
 
 
 function add(...args) {
     return args.reduce(function(acc, curr) {
         return acc + curr
+    })
+}
+function multiply(...args) {
+    return args.reduce(function(acc, curr) {
+        return acc * curr
     })
 }
 
